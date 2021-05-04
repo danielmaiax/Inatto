86c89b7c537402478e0133f1e0e763494f055862
daniel <manager@inatto.com>
2021-05-03 21:59:33 -0300

    t7600 inatto-phar

---
+++ build.php
@@ -0,0 +1,27 @@
+<?php
+
+//print phpinfo();
+//print (getcwd());
+//print_r($_SERVER);
+//exit;
+//print dirname(__FILE__);
+//exit;
+
+//
+//$srcRoot = __DIR__."/src/commerce";
+$buildRoot =  __DIR__."/build";
+
+// remove phar anterior
+if (file_exists($buildRoot . "/Inatto.phar")) unlink($buildRoot . "/Inatto.phar");
+
+//
+$phar = new Phar($buildRoot . "/Inatto.phar", FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::KEY_AS_FILENAME, "Inatto.phar");
+$phar->compressFiles(Phar::GZ);
+$phar->setDefaultStub(null, null);
+$phar->buildFromDirectory(dirname(__FILE__) . '/src/');
+$phar->setStub(($phar->createDefaultStub('Kernel/Core/Loader.php')));
+
+//
+//$phar->setStub($phar->createDefaultStub('cli/index.php', 'www/index.php'));
+//$phar->setStub($phar->createDefaultStub("index.php"));
+//copy($srcRoot . "/config.ini", $buildRoot . "/config.ini");
